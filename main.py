e51a928f2bf77c4207dca6677c4073a5fd2b621d
Meso Francis <118773373+MessoJ@users.noreply.github.com>
2025-07-04 18:07:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import utils
 import numpy as np
 
 #-------------------------------------------------------------------------------
-# Data loading. There is no need to edit code in this section.
+# Data loading.
 #-------------------------------------------------------------------------------
 
 train_data = utils.load_data('reviews_train.tsv')
@@ -18,14 +18,10 @@ dictionary = p1.bag_of_words(train_texts, remove_stopword=True)
 
 
 
-# For count features (second task):
+#  Count features:
 train_bow_features = p1.extract_bow_feature_vectors(train_texts, dictionary, binarize=False)
 val_bow_features = p1.extract_bow_feature_vectors(val_texts, dictionary, binarize=False)
 test_bow_features = p1.extract_bow_feature_vectors(test_texts, dictionary, binarize=False)
-
-#-------------------------------------------------------------------------------
-# Problem 5
-#-------------------------------------------------------------------------------
 
 toy_features, toy_labels = toy_data = utils.load_toy_data('toy_data.tsv')
 
@@ -45,10 +41,6 @@ plot_toy_results('Perceptron', thetas_perceptron)
 plot_toy_results('Average Perceptron', thetas_avg_perceptron)
 plot_toy_results('Pegasos', thetas_pegasos)
 
-#-------------------------------------------------------------------------------
-# Problem 7
-#-------------------------------------------------------------------------------
-
 T = 10
 L = 0.01
 
@@ -67,13 +59,9 @@ avg_peg_train_accuracy, avg_peg_val_accuracy = \
 print("{:50} {:.4f}".format("Training accuracy for Pegasos:", avg_peg_train_accuracy))
 print("{:50} {:.4f}".format("Validation accuracy for Pegasos:", avg_peg_val_accuracy))
 
-#-------------------------------------------------------------------------------
-# Problem 8
-#-------------------------------------------------------------------------------
-
 data = (train_bow_features, train_labels, val_bow_features, val_labels)
 
- # values of T and lambda to try
+ # values of T and lambda
 Ts = [1, 5, 10, 15, 25, 50]
 Ls = [0.001, 0.01, 0.1, 1, 10]
 
@@ -85,7 +73,7 @@ avg_pct_tune_results = utils.tune_avg_perceptron(Ts, *data)
 print('avg perceptron valid:', list(zip(Ts, avg_pct_tune_results[1])))
 print('best = {:.4f}, T={:.4f}'.format(np.max(avg_pct_tune_results[1]), Ts[np.argmax(avg_pct_tune_results[1])]))
 
-# fix values for L and T while tuning Pegasos T and L, respective
+# values for L and T while tuning Pegasos T and L,
 fix_L = 0.01
 peg_tune_results_T = utils.tune_pegasos_T(fix_L, Ts, *data)
 print('Pegasos valid: tune T', list(zip(Ts, peg_tune_results_T[1])))
@@ -102,14 +90,12 @@ utils.plot_tune_results('Pegasos', 'T', Ts, *peg_tune_results_T)
 utils.plot_tune_results('Pegasos', 'L', Ls, *peg_tune_results_L)
 
 #-------------------------------------------------------------------------------
-# Use the best method (perceptron, average perceptron or Pegasos) along with
+# UseD the best method ( Pegasos) along with
 # the optimal hyperparameters according to validation accuracies to test
-# against the test dataset. The test data has been provided as
+# against the test dataset. The test data has is as
 # test_bow_features and test_labels.
 #-------------------------------------------------------------------------------
-
-# Your code here
-# Train Pegasos with optimal hyperparameters (e.g., from tuning)
+# Train Pegasos with optimal hyperparameters ( from tuning)
 T_best = 10
 L_best = 0.01
 thetas_pegasos = p1.pegasos(train_bow_features, train_labels, T=T_best, L=L_best)
@@ -119,7 +105,7 @@ test_accuracy = p1.classifier_accuracy(p1.pegasos, train_bow_features, test_bow_
 print(f"Test accuracy for Pegasos (T={T_best}, L={L_best}): {test_accuracy:.4f}")
 
 #-------------------------------------------------------------------------------
-# Assign to best_theta, the weights (and not the bias!) learned by your most
+# Assign to best_theta, the weights (and not the bias!) learned by my most
 # accurate algorithm with the optimal choice of hyperparameters.
 #-------------------------------------------------------------------------------
 
